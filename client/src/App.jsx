401dd60a5fa5517c570f0a755cecdacdf1e18ebb
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-05-09 22:43:54 -0400

    done except for responsive sizing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,9 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import Carousel from './components/Carousel'
 import Footer from './components/Footer.jsx'
-import { BsSunFill, BsMoonFill } from 'react-icons/bs'
-
+import { BsSunFill, BsMoonFill, BsGithub, BsLinkedin } from 'react-icons/bs'
+import { FaFileDownload } from 'react-icons/fa'
+import Resume from './assets/document.pdf'
 
 function App() {
   const [theme, setTheme] = useState("Light")
@@ -23,13 +24,28 @@ function App() {
 
   return (
     <>
-    <div className="flex text-blue-600 dark:text-zinc-300 p-0 m-0 dark:bg-gray-800 pt-4 pb-12">
-    <button onClick={handleThemeSwitch}>{theme === 'dark' ? <BsSunFill className='text-3xl ml-2 text-cyan-400 hover:scale-110 hover:text-cyan-300'/> : <BsMoonFill className='text-3xl ml-2 hover:scale-110' />}</button> 
-    <div className="container">
-    <h1 className={theme === 'dark' ? 'neon' : 'text-6xl text-center ml-[11%] container heading'}>Kenan McKenzie</h1>
+    <div className="grid grid-cols-3 items-center text-gray-600 p-0 m-0 dark:bg-gray-800 pt-4 pb-12  ">
+      <div className='COL1 '>
+        <button onClick={handleThemeSwitch}>{theme === 'dark' ? <BsSunFill className='text-3xl ml-8 text-cyan-400 hover:scale-110 hover:text-cyan-300 hover:text-white'/> : <BsMoonFill className='dark:hover:text-white text-3xl ml-8 hover:scale-110 hover:text-gray-700' />}</button>
+      </div>
+      <div className="COL2 grid grid-cols-1 justify-content-center  text-center">
+        <h1 className={theme === 'dark' ? 'neon' : 'text-6xl heading font-sans'}>Kenan McKenzie</h1>
+      </div>
+      <div className='COL3 '>
+      <div className="flex justify-end gap-4 mr-8 dark:text-cyan-400 text-3xl ">
+        <a href={Resume} download='Resume.pdf'>
+          <FaFileDownload className='dark:hover:text-white hover:text-gray-700 text-3xl hover:scale-110 '/>
+        </a>
+        <a href='https://github.com/kwm0304' target='_blank'>
+          <BsGithub className='dark:hover:text-white text-3xl hover:text-gray-700 hover:scale-110'/>
+        </a>
+        <a href='https://www.linkedin.com/in/kenan-mckenzie-29102025a/' target='_blank'>
+          <BsLinkedin className='dark:hover:text-white text-3xl hover:text-gray-700 hover:scale-110' />
+        </a>
+      </div>
+      </div>
     </div>
-    </div>
-    <section className='bg-gray-350 dark:bg-slate-800 grid justify-center'>
+    <section className='bg-gray-350 dark:bg-slate-800 grid justify-center '>
       <Carousel />
       <Footer />
     </section>
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,9 +2,9 @@ import { BsFillTelephoneFill, BsFillEnvelopeFill } from "react-icons/bs"
 const Footer = () => {
 return(
   <>
-  <div className="flex gap-12 text-4xl dark:text-cyan-400 justify-center pb-3">
-    <a href="tel:704-796-4326"><BsFillTelephoneFill /></a>
-    <a href='mailto:mckenzie.kenan.90@gmail.com'><BsFillEnvelopeFill /></a>
+  <div className="flex gap-12 text-4xl  justify-center pb-8 h-full">
+    <a href="tel:704-796-4326" target='_blank'><BsFillTelephoneFill className="dark:text-cyan-400 dark:hover:text-white hover:scale-110 text-gray-600 hover:text-gray-700"/></a>
+    <a href='mailto:mckenzie.kenan.90@gmail.com' target='_blank'><BsFillEnvelopeFill className="dark:text-cyan-400 dark:hover:text-white hover:scale-110 text-gray-600 hover:text-gray-700"/></a>
   </div>
   </>
 )
